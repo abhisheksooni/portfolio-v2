bf4d682c29441e4f08817990875ebb1ebbb004b5
Abhishek Soni <abhisheksoni.as444@gmail.com>
2024-05-21 23:38:08 -0700

    fix navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { github, gmail, whatsapp } from '../Images/AllImagesExport';
 
 const Navbar = () => {
 
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <>
 
-      <div className='w-[100%] flex justify-end py-4 pr-10 cursor-pointer'>
+      <div className='w-[100%] flex justify-end py-4 pr-5 lg:pr-10 cursor-pointer '>
         <div
           onClick={() => {Setmanu(true)
             Setmanu(manu?false:true)
@@ -20,14 +21,21 @@ const Navbar = () => {
           <div className={` ${point?'bg-red-700':'bg-color3'} h-[11px] w-[11px] bg-color3 lg:mt-[4.5px] rounded-full`}></div>
         </div>
         {/* menu in pc  */}
-        <div  className={`${manu ? 'inline-block' : 'hidden'} absolute top-[90px] z-50 `}>
-          <div className="flex flex-wrap bg-color2 p-5 rounded-3xl gap-3">
-            <div className="bg-color3/50 w-[100px] h-[100px] rounded-2xl "></div>
-            <div className="bg-color3/50 w-[100px] h-[100px] rounded-2xl "></div>
-            <div className="bg-color3/50 w-[100px] h-[100px] rounded-2xl "></div>
+        <div  className={`${manu ? 'inline-block' : 'hidden'} w-[90%] md:w-fit ml-5 absolute top-[75px] lg:top-[90px] z-50 `}>
+          <div className="flex flex-wrap *:w-[72px] justify-evenly  lg:*:w-[85px] lg:*:h-[85px] *:rounded-full  bg-color2/60 p-5 rounded-3xl gap-3 backdrop-blur-sm">
+          <div className="bg-black p-3.5 lg:p-4  ">
+            <img src={github} alt="" />
+            </div>
+            <div className="bg-[#25D366] p-3.5 lg:p-4  ">
+              <img src={whatsapp} alt="" />
+            </div>
+            <div className="bg-[#EA4335] p-3.5 lg:p-4  ">
+            <img src={gmail} alt="" />
+            </div>
+            
           </div>
-          <div className="bg-color2 p-5 rounded-3xl mt-2">
-
+          <div className="bg-color2/60 p-5 text-center text-color3/40 backdrop-blur-sm rounded-3xl mt-2">
+          no notifications
           </div>
         </div>
       </div>
