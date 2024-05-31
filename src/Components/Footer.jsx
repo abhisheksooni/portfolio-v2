7b1616a8368d56b95e8cda670f6d9fea6334fb7b
Abhishek Soni <abhisheksoni.as444@gmail.com>
2024-05-31 05:58:07 -0700

    footer fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex justify-center gap-5 text-color3 text-xl'>
-        <span>Copy Right By </span>
-        <div className="">
-            <img className='w-[40px] h-[40px] rounded-full' src="" alt="" />
-        </div>
+    <div className='flex justify-center flex-col sm:flex-row items-center gap-5 text-color3 text-xl mb-6'>
+        <span className='hidden sm:grid'>Copy Right By </span>
+        <div className="avtar w-[40px] h-[40px] lg:w-[70px] lg:h-[70px] rounded-full hover:scale-110 transition-all duration-150  hover:w-[90px] cursor-pointer">   </div>
         <span>Abhishek soni </span>
     </div>
   )
